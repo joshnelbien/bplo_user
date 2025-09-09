2fa9a0c38fd2a0b218e1b0c51211620cfe73994b
JeremiahIT <jeremica03132001@gmail.com>
2025-09-09 11:46:30 +0800

    committtt

--- backend/db/model/announcements.js
+++ backend/db/model/announcements.js
@@ -16,6 +16,15 @@ const Announcements = sequelize.define("Announcements", {
     defaultValue: DataTypes.NOW,
     allowNull: false,
   },
+  createdBy: {
+    type: DataTypes.STRING, // Use STRING to store the creator's name
+    allowNull: false, // Ensure this is not null to prevent "Unknown" from appearing
+    defaultValue: "Admin", // Set a default value if not provided
+  },
+  attachedImageBlob: {
+    type: DataTypes.TEXT("long"), // Use TEXT("long") to store a potentially large base64 image string
+    allowNull: true, // This field can be null if no image is attached
+  },
 });
 
 module.exports = Announcements;
