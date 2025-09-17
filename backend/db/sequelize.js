6ee2e17059f3bdbabdc485162df8a79626409ca6
joshnelbien <pilaresjoshuel@gmail.com>
2025-09-17 09:05:16 +0800

    env local

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,49 +1,49 @@
-// // db/sequelize.js
-const { Sequelize } = require("sequelize");
-
-const {
-  SUPABASE_DB_HOST,
-  SUPABASE_DB_PORT,
-  SUPABASE_DB_NAME,
-  SUPABASE_DB_USER,
-  SUPABASE_DB_PASS,
-  SUPABASE_DB_SSL,
-} = process.env;
-
-const sequelize = new Sequelize(
-  SUPABASE_DB_NAME,
-  SUPABASE_DB_USER,
-  SUPABASE_DB_PASS,
-  {
-    host: SUPABASE_DB_HOST,
-    port: SUPABASE_DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl:
-        SUPABASE_DB_SSL === "true"
-          ? { require: true, rejectUnauthorized: false }
-          : false,
-    },
-    logging: false,
-  }
-);
-
-module.exports = { sequelize };
-
-// db/sequelize.js
+// supabase db connection
 // const { Sequelize } = require("sequelize");
 
-// const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SSL } = process.env;
+// const {
+//   SUPABASE_DB_HOST,
+//   SUPABASE_DB_PORT,
+//   SUPABASE_DB_NAME,
+//   SUPABASE_DB_USER,
+//   SUPABASE_DB_PASS,
+//   SUPABASE_DB_SSL,
+// } = process.env;
 
-// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl:
-//       DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
-//   },
-//   logging: false,
-// });
+// const sequelize = new Sequelize(
+//   SUPABASE_DB_NAME,
+//   SUPABASE_DB_USER,
+//   SUPABASE_DB_PASS,
+//   {
+//     host: SUPABASE_DB_HOST,
+//     port: SUPABASE_DB_PORT,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl:
+//         SUPABASE_DB_SSL === "true"
+//           ? { require: true, rejectUnauthorized: false }
+//           : false,
+//     },
+//     logging: false,
+//   }
+// );
 
 // module.exports = { sequelize };
+
+// localhost db connection
+const { Sequelize } = require("sequelize");
+
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SSL } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl:
+      DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
+  },
+  logging: false,
+});
+
+module.exports = { sequelize };
